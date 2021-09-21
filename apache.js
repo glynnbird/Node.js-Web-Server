24d4776da39216440d1594bbd0e74d446c7ce63b
Glynn Bird <glynn.bird@gmail.com>
2021-09-21 10:54:15 +0100

    update to get dependabot off my back

--- apache.js
+++ apache.js
@@ -1,15 +1,12 @@
 
 // we need the express framework
-var express = require('express');
-var app = express();
-
-// use compression where appropriate
-app.use(express.compress());
+const express = require('express')
+const app = express()
 
 // server out our static directory as static files
-app.use(express.static(__dirname+'/public'));
+app.use(express.static(__dirname+'/public'))
 
 // listen on port 8000
-app.listen(8000);
-console.log("Visit http://localhost:8000/");
+app.listen(8000)
+console.log("Visit http://localhost:8000/")
 
